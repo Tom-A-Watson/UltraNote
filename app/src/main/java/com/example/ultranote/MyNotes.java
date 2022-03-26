a2d9fea741b8d646b3d7f613fb8b48ea03380680
Tom Watson <thomasw2701@gmail.com>
2022-03-26 11:14:06 +0000

    Finished 'Add Note' button and notes RecyclerView. Modified manifest file and app resources.

--- app/src/main/java/com/example/ultranote/MyNotes.java
+++ app/src/main/java/com/example/ultranote/MyNotes.java
@@ -1,6 +1,10 @@
 package com.example.ultranote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyNotes extends AppCompatActivity {
@@ -10,5 +14,20 @@ public class MyNotes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mynotes);
 
+        ImageView createNote = (ImageView) findViewById(R.id.createNoteBtn);
+
+        createNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCreateNote();
+            }
+        });
+
     }
+
+    private void openCreateNote() {
+        Intent intent = new Intent(this, CreateNote.class);
+        startActivity(intent);
+    }
+
 }
