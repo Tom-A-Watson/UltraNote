c2290f044543451aa503e6ef242d4d93336b4859
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-04-13 15:52:14 +0100

    Started template for CreateNoteActivity.java (currently text note only) which entailed minor Java programming, and adding new drawables

--- app/src/main/java/com/example/ultranote/CreateNote.java
+++ app/src/main/java/com/example/ultranote/CreateNote.java
@@ -1,14 +1,30 @@
 package com.example.ultranote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateNote extends AppCompatActivity {
 
+    public static final int ADD_NOTE_REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.createnote);
+        setContentView(R.layout.createnotepage);
 
+        Button createTextNote = (Button) findViewById(R.id.createTextNoteBtn);
+        createTextNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        ADD_NOTE_REQUEST_CODE
+                );
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ultranote/CreateNoteActivity.java
+++ app/src/main/java/com/example/ultranote/CreateNoteActivity.java
@@ -3,6 +3,8 @@ package com.example.ultranote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class CreateNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        ImageView backBtn = findViewById(R.id.backButton);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
