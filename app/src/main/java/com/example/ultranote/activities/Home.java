f6bea319795293e80a52845ebdefc1ee69836b7e
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-04-16 23:32:00 +0100

    Two-column widget display added on the 'Home' page. This displays any note that is added to the database, however eventually it will only be favourited notes. Similar technique needed for 'My notes' page.

--- app/src/main/java/com/example/ultranote/activities/Home.java
+++ app/src/main/java/com/example/ultranote/activities/Home.java
@@ -1,16 +1,65 @@
 package com.example.ultranote.activities;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import com.example.ultranote.R;
+import java.util.ArrayList;
+import java.util.List;
+import adapters.NotesAdapter;
+import database.NotesDatabase;
+import entities.Note;
 
 public class Home extends AppCompatActivity {
+
+    private RecyclerView favsRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        favsRecyclerView = findViewById(R.id.favsRecyclerView);
+        favsRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        favsRecyclerView.setAdapter(notesAdapter);
+
+        getNotes();
+    }
+
+    private void getNotes() {
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                if (noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                }
+                favsRecyclerView.smoothScrollToPosition(0);
+            }
+        }
+
+        new GetNotesTask().execute();
     }
 }
