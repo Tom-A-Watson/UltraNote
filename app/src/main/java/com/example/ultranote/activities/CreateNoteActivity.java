abc05f8b2f06edc1a3335c82a37ae917e55d5516
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-04-21 22:42:38 +0100

    Added functionality for indicating note colour. The indicated colour will be equal to that of the button clicked.

--- app/src/main/java/com/example/ultranote/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/ultranote/activities/CreateNoteActivity.java
@@ -1,11 +1,11 @@
 package com.example.ultranote.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.AsyncTaskLoader;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -29,8 +29,7 @@ public class CreateNoteActivity extends AppCompatActivity {
 
     private EditText noteTitleInput, noteSubtitleInput, noteInput;
     private TextView textDateTime;
-    LinearLayout colourPickerLayout;
-    BottomSheetBehavior<LinearLayout> bottomSheetBehavior;
+    private View noteColourIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +44,11 @@ public class CreateNoteActivity extends AppCompatActivity {
             }
         });
 
-        colourPickerLayout = findViewById(R.id.colourPickerLayout);
-        bottomSheetBehavior = BottomSheetBehavior.from(colourPickerLayout);
-
         noteTitleInput = findViewById(R.id.noteTitleInput);
         noteSubtitleInput = findViewById(R.id.noteSubtitleInput);
         noteInput = findViewById(R.id.noteInput);
         textDateTime = findViewById(R.id.textDateTime);
+        noteColourIndicator = findViewById(R.id.noteColourIndicator);
 
         textDateTime.setText(
                 new SimpleDateFormat("EEEE dd MMMM yyyy HH:mm a",
@@ -67,7 +64,6 @@ public class CreateNoteActivity extends AppCompatActivity {
         });
 
         initColourPicker();
-
     }
 
     private void saveNote() {
@@ -107,10 +103,9 @@ public class CreateNoteActivity extends AppCompatActivity {
         new SaveNoteTask().execute();
     }
 
-    private void initColourPicker()
-    {
-
-
+    private void initColourPicker() {
+        final LinearLayout colourPickerLayout = findViewById(R.id.colourPickerLayout);
+        final BottomSheetBehavior<LinearLayout> bottomSheetBehavior = BottomSheetBehavior.from(colourPickerLayout);
 
         colourPickerLayout.findViewById(R.id.colourPickerText).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -122,6 +117,127 @@ public class CreateNoteActivity extends AppCompatActivity {
                 }
             }
         });
+
+        final ImageView imageColour1 = colourPickerLayout.findViewById(R.id.imageColour1);
+        final ImageView imageColour2 = colourPickerLayout.findViewById(R.id.imageColour2);
+        final ImageView imageColour3 = colourPickerLayout.findViewById(R.id.imageColour3);
+        final ImageView imageColour4 = colourPickerLayout.findViewById(R.id.imageColour4);
+        final ImageView imageColour5 = colourPickerLayout.findViewById(R.id.imageColour5);
+        final ImageView imageColour6 = colourPickerLayout.findViewById(R.id.imageColour6);
+        final ImageView imageColour7 = colourPickerLayout.findViewById(R.id.imageColour7);
+        final ImageView imageColour8 = colourPickerLayout.findViewById(R.id.imageColour8);
+        final ImageView imageColour9 = colourPickerLayout.findViewById(R.id.imageColour9);
+        final ImageView imageColour10 = colourPickerLayout.findViewById(R.id.imageColour10);
+        final ImageView imageColour11 = colourPickerLayout.findViewById(R.id.imageColour11);
+        final ImageView imageColour12 = colourPickerLayout.findViewById(R.id.imageColour12);
+        final ImageView imageColour13 = colourPickerLayout.findViewById(R.id.imageColour13);
+        final ImageView[] imageColours = new ImageView[]{imageColour1, imageColour2, imageColour3,
+                imageColour4, imageColour5, imageColour6, imageColour7, imageColour8, imageColour9,
+                imageColour10, imageColour11, imageColour12, imageColour13};
+
+        colourPickerLayout.findViewById(R.id.viewColour1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 1);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#333333"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 2);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#FF2929"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 3);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#FF5722"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 4);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#FF9800"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 5);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#FFE719"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 6);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#8BC34A"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 7);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#4CAF50"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 8);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#00BCD4"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 9);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#2196F3"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour10).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 10);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#3F51B5"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour11).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 11);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#673AB7"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour12).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 12);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#9C27B0"));
+            }
+        });
+        colourPickerLayout.findViewById(R.id.viewColour13).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickSelected(imageColours, 13);
+                noteColourIndicator.setBackgroundColor(Color.parseColor("#E91E63"));
+            }
+        });
+    }
+
+    private void tickSelected(ImageView[] imageColours, int colourNumber) {
+
+        for (int i = 0; i < imageColours.length; i++) {
+            if (i == colourNumber - 1) {
+                imageColours[i].setImageResource(R.drawable.ic_done);
+                continue;
+            }
+
+            imageColours[i].setImageResource(0);
+        }
     }
 
 }
