98e7e8fe2a15a38aa3176956f7f31db1186c8f44
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-04-26 19:47:19 +0100

    Made a good start on the 'add image' functionality although not working fully

--- app/src/main/java/com/example/ultranote/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/ultranote/activities/CreateNoteActivity.java
@@ -1,11 +1,21 @@
 package com.example.ultranote.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,6 +24,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.example.ultranote.R;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -25,7 +37,11 @@ public class CreateNoteActivity extends AppCompatActivity {
     private EditText noteTitleInput, noteSubtitleInput, noteInput;
     private TextView textDateTime;
     private View noteColourIndicator;
+    private ImageView noteImage;
     final Note note = new Note();
+
+    private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
+    private static final int REQUEST_CODE_SELECT_IMAGE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +61,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         noteInput = findViewById(R.id.noteInput);
         textDateTime = findViewById(R.id.textDateTime);
         noteColourIndicator = findViewById(R.id.noteColourIndicator);
+        noteImage = findViewById(R.id.noteImage);
 
         textDateTime.setText(
                 new SimpleDateFormat("EEEE dd MMMM yyyy HH:mm a",
@@ -56,6 +73,25 @@ public class CreateNoteActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 saveNote();
+            }
+        });
+
+        ImageView addImageBtn = findViewById(R.id.addImageButton);
+        addImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if  (ContextCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            CreateNoteActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_STORAGE_PERMISSION
+                    );
+                } else {
+                    selectImage();
+                }
+
             }
         });
 
@@ -249,4 +285,47 @@ public class CreateNoteActivity extends AppCompatActivity {
         }
     }
 
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                selectImage();
+            } else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    try {
+
+                        InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        noteImage.setImageBitmap(bitmap);
+                        noteImage.setVisibility(View.VISIBLE);
+
+                    } catch (Exception e) {
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
 }
