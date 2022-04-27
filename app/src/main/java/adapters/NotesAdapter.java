721a3e7d95791b22216b83597fb7d619d7cadb76
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-04-27 02:19:20 +0100

    Image saved in note is now displayed as the top of the widget on the Home page, with content below

--- app/src/main/java/adapters/NotesAdapter.java
+++ app/src/main/java/adapters/NotesAdapter.java
@@ -1,5 +1,6 @@
 package adapters;
 
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.ultranote.R;
+import com.makeramen.roundedimageview.RoundedImageView;
+
 import java.util.List;
 import entities.Note;
 
@@ -53,12 +56,15 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
 
         TextView textTitle, textSubtitle, textDateTime;
         LinearLayout widgetLayout;
+        RoundedImageView noteImage;
 
         NoteViewHolder(@NonNull View itemView) {
             super(itemView);
             textTitle = itemView.findViewById(R.id.textTitle);
             textSubtitle = itemView.findViewById(R.id.textSubtitle);
             textDateTime = itemView.findViewById(R.id.textDateTime);
+            widgetLayout = itemView.findViewById(R.id.widgetLayout);
+            noteImage = itemView.findViewById(R.id.noteImage);
         }
 
         void setNote(Note note) {
@@ -70,13 +76,19 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
             }
 
             textDateTime.setText(note.getDateTime());
-            widgetLayout = itemView.findViewById(R.id.widgetLayout);
 
             GradientDrawable gd = (GradientDrawable) widgetLayout.getBackground();
             if (note.getColour() != null) {
                 gd.setColor(Color.parseColor(note.getColour()));
             } else {
                 gd.setColor(Color.parseColor("#333333"));
+            }
+
+            if (note.getImagePath() != null) {
+                noteImage.setImageBitmap(BitmapFactory.decodeFile(note.getImagePath()));
+                noteImage.setVisibility(View.VISIBLE);
+            } else {
+                noteImage.setVisibility(View.GONE);
             }
         }
     }
