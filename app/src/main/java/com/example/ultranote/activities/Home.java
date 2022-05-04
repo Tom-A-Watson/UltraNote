54fd25d78ca97a923dff3136b026601ae7e52ac9
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-05-04 18:48:49 +0100

    Removed missing line in imports in Home.java

--- app/src/main/java/com/example/ultranote/activities/Home.java
+++ app/src/main/java/com/example/ultranote/activities/Home.java
@@ -18,7 +18,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
