b2ded8c91c2acb5a5a2afa93fbaf58fbf2579f73
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-05-05 15:47:56 +0100

    Slight tweak to the 'Quick Note' EditText's padding, and optimised code layout in NotesDatabase.java

--- app/src/main/java/database/NotesDatabase.java
+++ app/src/main/java/database/NotesDatabase.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-
 import dao.NoteDao;
 import entities.Note;
 
@@ -22,9 +21,9 @@ public abstract class NotesDatabase extends RoomDatabase {
                     "notes_db"
             ).build();
         }
+
         return notesDatabase;
     }
 
     public abstract NoteDao noteDao();
-
 }
